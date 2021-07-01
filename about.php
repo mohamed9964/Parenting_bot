0724823d97d7efd65ab10301ec26201b6b00fb91
Mohamed Hany <53129060+mohamed9964@users.noreply.github.com>
2021-07-01 21:58:08 +0200

    Update about.php

--- about.php
+++ about.php
@@ -54,8 +54,7 @@ include 'init.php';
             <div class="col-lg-7">
                 <div class="section_tittle_style_02">
                     <h2 class="title wow fadeInDown" data-wow-delay=".3s"> <span class="title_overlay_effect"> About Us</span></h2>
-                    <p class="description wow fadeInDown" data-wow-delay=".3s">Our goal is to replace traditional parents' ideas and beliefs with modern ideas based on scientific studies and
-                        successful experiences</p>
+                    <p class="description wow fadeInDown" data-wow-delay=".3s">Our project seeks to make a system that make the communication between the parents and their children easy.</p>
                 </div>
             </div>
         </div>
@@ -81,23 +80,11 @@ include 'init.php';
             <div class="col-lg-6 order-1 order-lg-2">
                 <div class="img_section">
                     <img src="layout/img/about_img_shape_2.png" alt="#" class="about_img_7" data-parallax="{&quot;x&quot;: 0, &quot;y&quot;: -50, &quot;rotateZ&quot;:0}" style="transform:translate3d(0px, -24.24px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0px, -24.24px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); ">
-                    <img src="layout/img/about_img_7.png" alt="#" class="about_img_6 img-fluid">
+                    <img src="layout/img/about_img_2.png" alt="#" class="about_img_6 img-fluid">
                     <img src="layout/img/about_img_shape_1.png" alt="#" class="about_img_5" data-parallax="{&quot;x&quot;: 0, &quot;y&quot;: 50, &quot;rotateZ&quot;:0}" style="transform:translate3d(0px, 17.76px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0px, 17.76px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); ">
                 </div>
             </div>
         </div>
-    </div>
-    <div class="about_page_animation_1">
-        <div data-parallax="{&quot;x&quot;: 2, &quot;y&quot;: 70, &quot;rotateZ&quot;:0}" style="transform:translate3d(0.999px, 33.965px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0.999px, 33.965px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="layout/img/icon/icon_5.png" alt="#"></div>
-    </div>
-    <div class="about_page_animation_2">
-        <div data-parallax="{&quot;x&quot;: 10, &quot;y&quot;: 80, &quot;rotateZ&quot;:0}" style="transform:translate3d(1.616px, 12.928px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(1.616px, 12.928px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="layout/img/icon/icon_6.png" alt="#"></div>
-    </div>
-    <div class="about_page_animation_3">
-        <div data-parallax="{&quot;x&quot;: 30, &quot;y&quot;: 60, &quot;rotateZ&quot;:0}" style="transform:translate3d(14.573px, 29.117px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(14.573px, 29.117px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="layout/img/icon/icon_7.png" alt="#"></div>
-    </div>
-    <div class="about_page_animation_4">
-        <div data-parallax="{&quot;x&quot;: 30, &quot;y&quot;: -50, &quot;rotateZ&quot;:0}" style="transform:translate3d(1.392px, -2.291px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(1.392px, -2.291px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="layout/img/icon/icon_8.png" alt="#"></div>
     </div>
 </section>
 <!-- about section part end -->
